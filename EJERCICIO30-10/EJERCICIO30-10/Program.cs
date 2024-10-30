dd45cffa6cef78ddfa21ee57ef96818f7faff3d6
YULIAN ALEXIS PAREJA ESTRADA <yulianpareja1128580@correo.itm.edu.co>
2024-10-30 14:59:38 -0500

    calcular matricula

--- EJERCICIO30-10/EJERCICIO30-10/Program.cs
+++ EJERCICIO30-10/EJERCICIO30-10/Program.cs
@@ -4,15 +4,25 @@ class Program
 {
     class Estudiante
     { public int Estrato {  get; set; }
-    public int Credito {  get; set; }
+    public int Creditos {  get; set; }
     public double ValorCredito { get; set; }
     public double PrecioMatricula { get; private set; }
-    public double Subcidio {  get; set; }
+    public double Subcidio {  get; private set; }
     
     public Estudiante(int ESTRATO, int CREDITOS, double VALORCREDITO)
-        {  Estrato = ESTRATO; Credito = CREDITOS; ValorCredito = VALORCREDITO;} 
+        {  Estrato = ESTRATO; Creditos = CREDITOS; ValorCredito = VALORCREDITO;} 
 
-    
+    public void CalcularMatricula() 
+        { if (Creditos <= 20) { PrecioMatricula = Creditos * ValorCredito; }
+        else { int CreditosExtra= Creditos-20; PrecioMatricula=20*ValorCredito;
+        PrecioMatricula += CreditosExtra * ValorCredito * 2;
+     if (Estrato == 1) { PrecioMatricula *= 0.2; }
+     if (Estrato == 2) { PrecioMatricula *= 0.5; }
+     if (Estrato == 3) { PrecioMatricula *= 0.7; }
+
+            }
+        }
+            
 
 
 
