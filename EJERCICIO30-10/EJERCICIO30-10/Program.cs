207570c8ec2647925ea5cf0c32db258e9d8428e5
YULIAN ALEXIS PAREJA ESTRADA <yulianpareja1128580@correo.itm.edu.co>
2024-10-30 17:16:25 -0500

    EJECUTAMOS

--- EJERCICIO30-10/EJERCICIO30-10/Program.cs
+++ EJERCICIO30-10/EJERCICIO30-10/Program.cs
@@ -18,9 +18,9 @@ class Program
         else { int CreditosExtra= Creditos-20; PrecioMatricula=20*ValorCredito;
         PrecioMatricula = PrecioMatricula + (CreditosExtra * ValorCredito * 2);}
 
-     if (Estrato == 1) { PrecioMatricula *= 0.2; }
+     if (Estrato == 1) { PrecioMatricula *= 0.3; }
      else if (Estrato == 2) { PrecioMatricula *= 0.5; }
-     else if (Estrato == 3) { PrecioMatricula *= 0.7; }}
+     else if (Estrato == 3) { PrecioMatricula *= 0.8; }}
     
     public void CalcularSubsidio()
         { if (Estrato == 1) { Subsidio = 200000; }
@@ -31,7 +31,7 @@ class Program
       Console.WriteLine($"el valor del subsidio es: $ {Subsidio} ");}
      
         
-     static void main()
+     static void Main()
       { bool continuar = true;
         while (continuar)
       { Console.WriteLine("Ingrese el estrato del estudiante (1_3):");
@@ -39,7 +39,7 @@ class Program
         Console.WriteLine("Ingrese la cantidad de creditos a tomar:");
         int Creditos = int.Parse(Console.ReadLine());
         Console.WriteLine("Ingrese el valor del credito:");
-        double ValorCredito = int.Parse(Console.ReadLine());
+        double ValorCredito = double.Parse(Console.ReadLine());
 
      Estudiante estudiante = new Estudiante(Estrato, Creditos, ValorCredito);
     estudiante.CalcularMatricula();
@@ -48,10 +48,4 @@ class Program
                 Console.Write("¿Quieres calcular la matricula de otro estudiante? (s/n): ");
                 string respuesta = Console.ReadLine().ToLower();
                 continuar = respuesta == "s"; }}}
-            
-
-
-
-
-
-    }
+            }
