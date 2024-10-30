eb436421a78f71bf3638716f07086822738af291
YULIAN ALEXIS PAREJA ESTRADA <yulianpareja1128580@correo.itm.edu.co>
2024-10-30 14:21:50 -0500

    ASIGNAMOS PROPIEDADES

--- EJERCICIO30-10/EJERCICIO30-10/Program.cs
+++ EJERCICIO30-10/EJERCICIO30-10/Program.cs
@@ -2,5 +2,18 @@
 
 class Program
 {
-    class
+    class ESTUDIANTE
+    { public int ESTRATO {  get; set; }
+    public int CREDITOS {  get; set; }
+    public double VALORCREDITO { get; set; }
+    public double PRECIOMATRICULA { get; private set; }
+    public double SUBCIDIO {  get; set; }
+
+
+
+
+
+
+
+    }
 }
