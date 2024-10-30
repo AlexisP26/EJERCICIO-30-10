a8c168e6173658441ba5e06c790b8d20f12a3100
YULIAN ALEXIS PAREJA ESTRADA <yulianpareja1128580@correo.itm.edu.co>
2024-10-30 11:48:38 -0500

    EMPECEMOS

---
+++ EJERCICIO30-10/EJERCICIO30-10/Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    public static void Main(string[] args)
+    {
+        NewMethod();
+
+        static void NewMethod()
+        {
+            Console.WriteLine("Hello, World!");
+
+
+        }
+    }
+}
